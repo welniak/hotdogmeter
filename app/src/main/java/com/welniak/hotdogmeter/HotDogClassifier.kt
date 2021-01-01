5e02fdf6e523c27c2d9b4c2a0158d025d7ef238f
welniak <maciejsurmacz@gmail.com>
2021-01-01 14:11:20 +0100

    Refactor hot dog classifier class to avoid creating the image classfier at each frame

--- app/src/main/java/com/welniak/hotdogmeter/HotDogClassifier.kt
+++ app/src/main/java/com/welniak/hotdogmeter/HotDogClassifier.kt
@@ -5,14 +5,20 @@ import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.task.vision.classifier.Classifications
 import org.tensorflow.lite.task.vision.classifier.ImageClassifier
 
-internal class HotDogClassifier(
-    private val context: Context
-) {
+internal class HotDogClassifier(context: Context) {
+
+    private val classifierOptions = ImageClassifier.ImageClassifierOptions
+        .builder()
+        .setScoreThreshold(SCORE_THRESHOLD)
+        .build()
+
+    private val imageClassifier by lazy {
+        ImageClassifier.createFromFileAndOptions(context, MODEL_PATH, classifierOptions)
+    }
 
     fun getHotDogScore(image: TensorImage): Float {
         var hotDogScore = 0.0f
-        ImageClassifier.createFromFileAndOptions(context, MODEL_PATH, classifierOptions)
-            .classify(image)
+            imageClassifier.classify(image)
             .forEach { classifications ->
                 hotDogScore = classifications.toHotDogScore()
             }
@@ -26,11 +32,6 @@ internal class HotDogClassifier(
         }
         return 0.0f
     }
-
-    private val classifierOptions = ImageClassifier.ImageClassifierOptions
-        .builder()
-        .setScoreThreshold(SCORE_THRESHOLD)
-        .build()
 
     private companion object {
         const val MODEL_PATH = "mobilenet_v1_1.0_224_quant.tflite"
